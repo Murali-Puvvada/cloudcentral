f52210853df6ecf2d9b71a9f99b284d59412138f
Murali Puvvada <saipuvvada81299@gmail.com>
2024-12-20 18:06:12 +0100

    Create a base repo using T3 Stack

---
+++ src/app/page.tsx
@@ -0,0 +1,28 @@
+import Link from "next/link";
+
+import { LatestPost } from "@/app/_components/post";
+import { auth } from "@/server/auth";
+import { api, HydrateClient } from "@/trpc/server";
+
+export default async function Home() {
+  const hello = await api.post.hello({ text: "from tRPC" });
+  const session = await auth();
+
+  if (session?.user) {
+    void api.post.getLatest.prefetch();
+  }
+
+  return (
+    <HydrateClient>
+      <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
+        <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
+          <h1 className="text-5xl font-extrabold tracking-tight sm:text-[5rem]">
+            Cloud Central
+          </h1>
+          <h3>Central Hub to seamlessly organize your Cloud Storage</h3>
+          <h5 className="flex flex-row items-center">Coming Soon</h5>
+        </div>
+      </main>
+    </HydrateClient>
+  );
+}
